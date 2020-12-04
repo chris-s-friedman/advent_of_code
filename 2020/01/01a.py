822946929507e623d3c22b34520e35202945539b
Chris Friedman <chris.s.friedman@gmail.com>
2020-12-03 21:00:32 -0500

    :lock: hide session info

--- 2020/01/01a.py
+++ 2020/01/01a.py
@@ -1,33 +1,33 @@
 # https://adventofcode.com/2020/day/1
-
-
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv("/Users/chris/Documents/projects/advent_of_code/2020/.env")
 # here's where the input list of numbers comes from
-input_url = 'https://adventofcode.com/2020/day/1/input'
+input_url = "https://adventofcode.com/2020/day/1/input"
 
 # the list of numbers generated is unique to each user - my session ID gives me
 # my list.
-session_id = ***REMOVED***  # noqa
+session_id = os.getenv("day1_session_id")
 # get the list of numbers
-r = requests.get(input_url, headers={'Cookie': f'session={session_id}'})
+r = requests.get(input_url, headers={"Cookie": f"session={session_id}"})
 
 # turn the text list (e.g. '1234\n5678\n9876\n') into a list that has non-empty
 # strings. Note that i drop the last item. this is because the string ends with
 # a newline.
 numbers = list(r.text.split("\n"))[:-1]
-s
 # figure out what two numbers add to 2020
 
 
 def check_sum(nums, k):
-    '''
+    """
     Test if a list of numbers add up to a certain value. if two of those
     numbers add up to a value, k, multiply those numbers together.
     Otherwise, return False
-    '''
+    """
     for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
+        for j in range(i + 1, len(nums)):
             if int(nums[i]) + int(nums[j]) == k:
                 return int(nums[i]) * int(nums[j])
     return False
--- 2020/01/01b.py
+++ 2020/01/01b.py
@@ -1,18 +1,17 @@
 # https://adventofcode.com/2020/day/1
-
-
 import requests
-import itertools
-import numpy as np
+from dotenv import load_dotenv
+import os
 
+load_dotenv("/Users/chris/Documents/projects/advent_of_code/2020/.env")
 # here's where the input list of numbers comes from
-input_url = 'https://adventofcode.com/2020/day/1/input'
+input_url = "https://adventofcode.com/2020/day/1/input"
 
 # the list of numbers generated is unique to each user - my session ID gives me
 # my list.
-session_id = ***REMOVED***  # noqa
+session_id = os.getenv("day1_session_id")
 # get the list of numbers
-r = requests.get(input_url, headers={'Cookie': f'session={session_id}'})
+r = requests.get(input_url, headers={"Cookie": f"session={session_id}"})
 
 # turn the text list (e.g. '1234\n5678\n9876\n') into a list that has non-empty
 # strings. Note that i drop the last item. this is because the string ends with
@@ -22,11 +21,11 @@ numbers = list(map(int, list(r.text.split("\n"))[:-1]))
 
 
 def check_sum(nums, group_size, k):
-    '''
+    """
     Test if a list of numbers add up to a certain value. if two of those
     numbers add up to a value, k, multiply those numbers together.
     Otherwise, return False
-    '''
+    """
     combinations = list(itertools.combinations(nums, group_size))
     for combination in combinations:
         if sum(combination) == k:
