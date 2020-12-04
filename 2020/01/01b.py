0a1b3b78297a68b9d514b999ee7072c7ad07563b
Chris Friedman <chris.s.friedman@gmail.com>
2020-12-03 20:41:42 -0500

    :tada: make blank files for each day!

--- 2020/01/01b.py
+++ 2020/01/01b.py
@@ -10,7 +10,7 @@ input_url = 'https://adventofcode.com/2020/day/1/input'
 
 # the list of numbers generated is unique to each user - my session ID gives me
 # my list.
-session_id = ***REMOVED***  # noqa
+session_id = '53616c7465645f5f0f14c41326a13662c75d3283649783725f0f65ab79b664afe94d133bcd5bee27e8c870446613abb1'  # noqa
 # get the list of numbers
 r = requests.get(input_url, headers={'Cookie': f'session={session_id}'})
 
